23ba88f5b7b7f6161570344439516c4ec29ba00d
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2021-01-21 20:34:13 -0500

    Moved some code from employeecontainer to app.js so I can set it up properly. Also added more components. Getting warning assign object to a variable before exporting as a module default

--- src/components/EmployeeContainer.js
+++ src/components/EmployeeContainer.js
@@ -1,20 +1,79 @@
-import React from 'react'
+import React, { Component } from "react";
+// import React from 'react'
+import Container from "./Container.js"
+import Row from "./Row.js"
+import Col from "./Col.js"
 import LandingPage from "./LandingPage"
-
 import SearchForm from "./SearchForm.js"
+import API from "../utils/API"
 
+// export default function EmployeeContainer() {
+//     return (
+//         <div>
+//             <LandingPage />
+//             <SearchForm />
+//         </div>
+//     )
+// }
 
-export default function EmployeeContainer() {
-    return (
-        <div>
-            <LandingPage />
-            <SearchForm />
-        </div>
-    )
-}
+// Referenced week 19 act 20 for comparable file
+class EmployeeContainer extends Component {
+    state = {
+      result: {},
+      search: ""
+    };
+  
+    // When this component mounts, search for employees
+    componentDidMount() {
+      this.searchEmployees("employees");
+    }
+  
+    searchEmployees = query => {
+      API.search(query)
+        .then(res => this.setState({ result: res.data }))
+        .catch(err => console.log(err));
+    };
+  
+    handleInputChange = event => {
+      const value = event.target.value;
+      const name = event.target.name;
+      this.setState({
+        [name]: value
+      });
+    };
+  
+    // When the form is submitted, search the random user API for the value of `this.state.search`
+    handleFormSubmit = event => {
+      event.preventDefault();
+      this.searchEmployees(this.state.search);
+    };
+  
+    render() {
+      return (
+        <Container>
+          <Row>
+          <Col size="sm-12">
+              <LandingPage>
 
+              </LandingPage>
+            </Col>
+          </Row>
+          <Row>
+          <Col size="sm-12">
+                <SearchForm
+                  value={this.state.search}
+                  handleInputChange={this.handleInputChange}
+                  handleFormSubmit={this.handleFormSubmit}
+                />
+            </Col>
+          </Row>
+        </Container>
+      );
+    }
+  }
 
 // Table
+
 
 <table class="table">
   <thead class="thead-dark">
@@ -46,3 +105,5 @@ export default function EmployeeContainer() {
     </tr>
   </tbody>
 </table>
+
+export default EmployeeContainer;
