d0120d41adef3c6732942ddca373670561af17d8
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2021-01-22 23:42:12 -0500

    updated api, but it still want s me to set it as a variable

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,10 +1,34 @@
 import axios from "axios";
 
-const URL = "https://randomuser.me/api/?results=200"
-
-const axiosAPI = {
-  search: function() {
-    return axios.get(URL);
-  }
+export default {
+    getData: () => axios.get("https://randomuser.me/api/?results=200")
 }
-export default axiosAPI;
+
+// Random User API. When the user loads the page, a table of employees should render
+// const URL = "https://randomuser.me/api/?results=100&inc=picture,name,email,phone";
+
+
+// export default {
+//   search: function() {
+//     return axios.get(URL);
+//   }
+// };
+
+// import axios from "axios";
+
+// // Random User API. When the user loads the page, a table of employees should render
+// const URL = "https://randomuser.me/";
+
+// React wants us to create this object as a variable before we try to export it
+
+
+
+// one way
+// const URL = "https://randomuser.me/api/"
+
+// const axiosAPI = {
+//   search: function() {
+//     return axios.get(URL);
+//   }
+// }
+// export default axiosAPI;
